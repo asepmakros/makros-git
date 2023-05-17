6eca2cdd3695714659b2c1f405d9605443cb06d9
asepmakros <056207@gmail.com>
2023-05-17 09:53:23 +0700

    menambah list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
     <h1>belajar git makros</h1>
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nobis distinctio repellendus, numquam quidem iusto error consequatur quae ut expedita autem quod blanditiis corporis accusantium sit aliquid voluptatibus pariatur quos.</p>
+    <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    </ul>
 </body>
 </html>
